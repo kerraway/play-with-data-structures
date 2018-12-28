81afca3b7a5c5463a1018c4da97380af061b3974
kerraway <geekxiaoke@gmail.com>
2018-12-28 16:54:17 +0800

    *feature) add Sum and SumTest for understanding recursion.

---
+++ src/main/java/com/github/kerraway/list/linked/v3/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * In the package, Recursion will be discussed.
+ * First of all, it's a simple recursion algorithm about getting sum of array.
+ * Then a linked list using recursion will be implemented.
+ *
+ * @author kerraway
+ * @date 2018/12/28
+ */
+package com.github.kerraway.list.linked.v3;
---
+++ src/test/java/com/github/kerraway/list/linked/v3/SumTest.java
@@ -0,0 +1,29 @@
+package com.github.kerraway.list.linked.v3;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author kerraway
+ * @date 2018/12/28
+ */
+public class SumTest {
+
+  @Test
+  public void test() {
+    final Random random = new Random();
+    int total = random.nextInt(100) + 100;
+    int[] arr = new int[total];
+    for (int i = 0; i < total; i++) {
+      arr[i] = random.nextInt(100);
+    }
+
+    int sum1 = new Sum().sumByTraverse(arr);
+    int sum2 = new Sum().sumByRecursive(arr);
+    assertEquals(sum1, sum2);
+  }
+
+}
