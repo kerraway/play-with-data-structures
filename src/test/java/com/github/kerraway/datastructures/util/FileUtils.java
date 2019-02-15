ffae49ebfed926977ae8cbc2344d7955f3761413
kerraway <geekxiaoke@gmail.com>
2019-02-15 14:28:06 +0800

    *feature) update FileUtils.

--- src/test/java/com/github/kerraway/datastructures/util/FileUtils.java
+++ src/test/java/com/github/kerraway/datastructures/util/FileUtils.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -46,6 +49,67 @@ public class FileUtils {
     Objects.requireNonNull(filePath, "filePath must not be null.");
 
     return read(filePath, StandardCharsets.UTF_8);
+  }
+
+  /**
+   * Reads file and returns words.
+   *
+   * @param filePath must not be {@literal null}.
+   * @param encoding must not be {@literal null}.
+   * @return List<String>
+   */
+  public static List<String> readWords(String filePath, Charset encoding) {
+    Objects.requireNonNull(filePath, "filePath must not be null.");
+    Objects.requireNonNull(encoding, "encoding must not be null.");
+
+    String str = read(filePath);
+    if (str == null || str.isEmpty()) {
+      return Collections.emptyList();
+    }
+    List<String> words = new ArrayList<>();
+    int start = indexOfFirstLetter(str, 0);
+    int cursor = start + 1;
+    while (cursor < str.length()) {
+      if (!Character.isLetter(str.charAt(cursor))) {
+        String word = str.substring(start, cursor);
+        words.add(word);
+        start = indexOfFirstLetter(str, cursor);
+        cursor = start + 1;
+      } else {
+        cursor++;
+      }
+    }
+    return words;
+  }
+
+  /**
+   * Reads file and returns words.
+   *
+   * @param filePath must not be {@literal null}.
+   * @return List<String>
+   */
+  public static List<String> readWords(String filePath) {
+    Objects.requireNonNull(filePath, "filePath must not be null.");
+
+    return readWords(filePath, StandardCharsets.UTF_8);
+  }
+
+  /**
+   * Get the index of first letter.
+   *
+   * @param str   must not be {@literal null}.
+   * @param start
+   * @return int
+   */
+  private static int indexOfFirstLetter(String str, int start) {
+    Objects.requireNonNull(str, "str must not be null.");
+
+    for (int i = start; i < str.length(); i++) {
+      if (Character.isLetter(str.charAt(i))) {
+        return i;
+      }
+    }
+    return str.length();
   }
 
   /**
